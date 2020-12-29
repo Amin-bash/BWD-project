6f988dd6515e4ca163fbd44a33b1df7f3ee1cb02
amin-bash <power.ameen99@gmail.com>
2020-12-29 12:32:07 +0200

    add orders tb

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import OrderTable from "./components/Table";
+import OrderTable from './components/Table';
 
 function App() {
-  const [state, setState] = useState({
-    data: []
-  })
+	const [ state, setState ] = useState({
+		data: []
+	});
 
-  const APIOrders = "http://51.15.242.149:8585/orders?status=active";
-  useEffect(() => {
-    // get orders
-    fetch(APIOrders)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setState(prevState => {
-            prevState.data = result;
-            return {...prevState}
-          })
-          console.log(state.data);
-
-        },
-        (error) => {
-         console.log(error);
-        }
-      )
-  }, [])
-  return (
-    <div className="App">
-      <h1>BWD project is ready</h1>
-      <OrderTable data={state.data} />
-    </div>
-  );
+	const APIOrders = 'http://51.15.242.149:8585/orders?status=active';
+	useEffect(() => {
+		// get orders
+		fetch(APIOrders).then((res) => res.json()).then(
+			(result) => {
+				setState((prevState) => {
+					prevState.data = result;
+					return { ...prevState };
+				});
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}, []);
+	return (
+		<div className="App">
+			<h1>BWD project is ready</h1>
+			<OrderTable data={state.data} />
+		</div>
+	);
 }
 
 export default App;
