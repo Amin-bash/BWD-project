eb468c1d30dd21020768e6c7c9426b0b048e6b09
amin-bash <power.ameen99@gmail.com>
2020-12-29 22:10:08 +0200

    remove hhtps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 import OrderTable from './components/Table';
+import axios from 'axios';
+
 
 function App() {
 	const [ state, setState ] = useState({
 		data: []
 	});
 
-	const APIOrders = 'https://51.15.242.149:8585/orders?status=active';
+	const APIOrders = 'http://51.15.242.149:8585/orders?status=active';
 	useEffect(() => {
 		// get orders
 		fetch(APIOrders).then((res) => res.json()).then(
@@ -20,7 +22,8 @@ function App() {
 			(error) => {
 				console.log(error);
 			}
-		);
+    );
+    
 	}, []);
 	return (
 		<div className="App">
