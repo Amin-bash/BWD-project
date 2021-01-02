bff30935c92e1f1793b5108c42c608873cdc2785
amin-bash <power.ameen99@gmail.com>
2021-01-02 12:38:32 +0200

    make the table sortable and hide side and close at

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 import OrderTable from './components/Table';
-
+import orderBy from 'lodash/orderBy';
 
 function App() {
 	const [ state, setState ] = useState({
 		activeTrades: {
 			trades: [],
-			pricePrecision: "",
-			qtyPrecision: "",
+			pricePrecision: '',
+			qtyPrecision: '',
+			showClosedAt: false,
+			showSide: true
 		},
 		closedTrades: {
 			trades: [],
-			pricePrecision: "",
-			qtyPrecision: "",
+			pricePrecision: '',
+			qtyPrecision: '',
+			showClosedAt: true,
+			showSide: false
 		},
+		columnToSort: '',
+		sortDirection: 'desc'
 	});
 
 	const activeTradesEndpoint = 'http://51.15.242.149:8585/orders?status=active';
@@ -24,7 +30,18 @@ function App() {
 		fetch(activeTradesEndpoint).then((res) => res.json()).then(
 			(result) => {
 				setState((prevState) => {
-					prevState.activeTrades.trades = result.orders;
+					prevState.activeTrades.trades = result.orders.reduce((acc, order) => {
+						const created_params = order.created_at.split(' ');
+						const converted_params = order.converted_sell_limit_at.split(' ');
+						acc.push({
+							...order,
+							created_at: created_params[0],
+							created_time: created_params[1],
+							converted_sell_limit_at: converted_params[0],
+							converted_time: converted_params[1]
+						});
+						return acc;
+					}, []);
 					prevState.activeTrades.pricePrecision = result.price_precision;
 					prevState.activeTrades.qtyPrecision = result.qty_precision;
 					return { ...prevState };
@@ -33,13 +50,28 @@ function App() {
 			(error) => {
 				console.log(error);
 			}
-        );
+		);
 
 		// closed trades
 		fetch(closedTradesEndpoint).then((res) => res.json()).then(
 			(result) => {
+				console.log(result);
 				setState((prevState) => {
-					prevState.closedTrades.trades = result.orders;
+					prevState.closedTrades.trades = result.orders.reduce((acc, order) => {
+						const created_params = order.created_at.split(' ');
+						const converted_params = order.converted_sell_limit_at.split(' ');
+						const closed_params = order.closed_at.split(' ');
+						acc.push({
+							...order,
+							created_at: created_params[0],
+							created_time: created_params[1],
+							converted_sell_limit_at: converted_params[0],
+							converted_time: converted_params[1],
+							closed_at: closed_params[0],
+							closed_time: closed_params[1]
+						});
+						return acc;
+					}, []);
 					prevState.closedTrades.pricePrecision = result.price_precision;
 					prevState.closedTrades.qtyPrecision = result.qty_precision;
 					return { ...prevState };
@@ -49,15 +81,32 @@ function App() {
 				console.log(error);
 			}
 		);
-
 	}, []);
+
+	const invertDirection = {
+		asc: 'desc',
+		desc: 'asc'
+	};
+
+	const handleSort = (columnName) => {
+		setState((prevState) => {
+			const newState = { ...prevState };
+			newState.columnToSort = columnName;
+			newState.sortDirection = invertDirection[newState.sortDirection];
+			return { ...newState };
+		});
+	};
+
 	return (
 		<div className="App">
 			<h1>BWD project</h1>
 			<div>
 				<h3>Active Trades</h3>
-			    <OrderTable
-					trades={state.activeTrades.trades}
+				<OrderTable
+					handleSort={handleSort}
+					showClosedAt={state.activeTrades.showClosedAt}
+					showSide={state.activeTrades.showSide}
+					trades={orderBy(state.activeTrades.trades, [ state.columnToSort ], [ state.sortDirection ])}
 					pricePrecision={state.activeTrades.pricePrecision}
 					qtyPrecision={state.activeTrades.qtyPrecision}
 				/>
@@ -65,7 +114,10 @@ function App() {
 			<div>
 				<h3>Closed Trades</h3>
 				<OrderTable
-					trades={state.closedTrades.trades}
+					handleSort={handleSort}
+					showClosedAt={state.closedTrades.showClosedAt}
+					showSide={state.closedTrades.showSide}
+					trades={orderBy(state.closedTrades.trades, [ state.columnToSort ], [ state.sortDirection ])}
 					pricePrecision={state.closedTrades.pricePrecision}
 					qtyPrecision={state.closedTrades.qtyPrecision}
 				/>
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -9,60 +9,94 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import './styles.scss';
 
-
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
+	table: {
+		minWidth: 650
+	}
 });
 
 const OrderTable = (props) => {
-  const classes = useStyles();
-  // key={md5(JSON.stringify(row))}
+	const classes = useStyles();
 
-  const rowColor = isPublished => {
-    if (isPublished === "1") {
-      return '#CCF8EB';
-    }
-    return '#F4F7F7';
-  }
+	const rowColor = (isPublished) => {
+		return isPublished === '1' ? '#CCF8EB' : '#F4F7F7';
+	};
 
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">Nr</TableCell>
-            <TableCell align="right">Create Date</TableCell>
-            <TableCell align="right">Side</TableCell>
-            <TableCell align="right">Exec Type</TableCell>
-            <TableCell align="right">Open Price</TableCell>
-            <TableCell align="right">Close Price</TableCell>
-            <TableCell align="right">Quantity</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="right">Convert Sell Date</TableCell>
-            <TableCell align="right">Closed Date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.trades.map((row, i) => (
-            <TableRow style={{ 'backgroundColor': rowColor(row.is_published) }}>
-              <TableCell align="right">{i + 1}</TableCell>
-              <TableCell align="right">{row.created_at}</TableCell>
-              <TableCell align="right">{row.side}</TableCell>
-              <TableCell align="right">{row.open_type} / {row.close_type}</TableCell>
-              <TableCell align="right">{parseFloat(row.open_ppu).toFixed(props.pricePrecision)}</TableCell>
-              <TableCell align="right">{parseFloat(row.close_ppu).toFixed(props.pricePrecision)}</TableCell>
-              <TableCell align="right">{parseFloat(row.qty).toFixed(props.qtyPrecision)}</TableCell>
-              <TableCell align="right">{(row.qty * row.open_ppu).toFixed(2)}</TableCell>
-              <TableCell align="right">{row.converted_sell_limit_at}</TableCell>
-              <TableCell align="right">{row.closed_at}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
+	return (
+		<TableContainer component={Paper}>
+			<Table className={classes.table} size="small" aria-label="a dense table">
+				<TableHead>
+					<TableRow>
+						<TableCell align="right">Nr</TableCell>
+						<TableCell onClick={() => props.handleSort('created_at')} align="right">
+							Create Date
+						</TableCell>
+						{props.showSide && (
+							<TableCell onClick={() => props.handleSort('side')} align="right">
+								Side
+							</TableCell>
+						)}
+						<TableCell onClick={() => props.handleSort('open_type')} align="right">
+							Exec Type
+						</TableCell>
+						<TableCell onClick={() => props.handleSort('open_price')} align="right">
+							Open Price
+						</TableCell>
+						<TableCell onClick={() => props.handleSort('close_price')} align="right">
+							Close Price
+						</TableCell>
+						<TableCell onClick={() => props.handleSort('qty')} align="right">
+							Quantity
+						</TableCell>
+						<TableCell onClick={() => props.handleSort('qty')} align="right">
+							Amount
+						</TableCell>
+						<TableCell onClick={() => props.handleSort('converted_sell_limit_at')} align="right">
+							Convert Sell Date
+						</TableCell>
+						{props.showClosedAt && (
+							<TableCell onClick={() => props.handleSort('closed_at')} align="right">
+								Closed Date
+							</TableCell>
+						)}
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{props.trades.map((row, i) => (
+						<TableRow style={{ backgroundColor: rowColor(row.is_published) }}>
+							<TableCell align="right">{i + 1}</TableCell>
+							<TableCell align="right">
+								<p>{row.created_at}</p>
+								<p>{row.created_time}</p>
+							</TableCell>
+							{props.showSide && <TableCell align="right">{row.side}</TableCell>}
+							<TableCell align="right">
+								{row.open_type} / {row.close_type}
+							</TableCell>
+							<TableCell align="right">
+								{parseFloat(row.open_price).toFixed(props.pricePrecision)}
+							</TableCell>
+							<TableCell align="right">
+								{parseFloat(row.close_price).toFixed(props.pricePrecision)}
+							</TableCell>
+							<TableCell align="right">{parseFloat(row.qty).toFixed(props.qtyPrecision)}</TableCell>
+							<TableCell align="right">{(row.qty * row.open_price).toFixed(2)}</TableCell>
+							<TableCell align="right">
+								<p>{row.converted_sell_limit_at}</p>
+								<p>{row.converted_time}</p>
+							</TableCell>
+							{props.showClosedAt && (
+								<TableCell align="right">
+									<p>{row.closed_at}</p>
+									<p>{row.closed_time}</p>
+								</TableCell>
+							)}
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+};
 
 export default OrderTable;
